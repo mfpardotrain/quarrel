7f72e6d60e989c736e0192c733151f0cb8347d12
mfpardotrain <pardotrain@gmail.com>
2022-02-28 02:43:57 -0500

    it works

---
+++ conf/build/precache-manifest.9a2cc8b898b4a1903f050973720d88a6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "079bf710d349f68a7c0513b06050fd98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c402918e719f077d670b",
+    "url": "/static/css/main.6e0bae17.chunk.css"
+  },
+  {
+    "revision": "478fa4346812cb06a6c0",
+    "url": "/static/js/2.488966fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.488966fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c402918e719f077d670b",
+    "url": "/static/js/main.25557cf4.chunk.js"
+  },
+  {
+    "revision": "e3c294d13d90cb419c58",
+    "url": "/static/js/runtime-main.b648a9c1.js"
+  },
+  {
+    "revision": "3e9431dc12aafa44ac9fa3b6712500e8",
+    "url": "/static/media/brown-logo.3e9431dc.svg"
+  },
+  {
+    "revision": "91ddc8b39db8fc41fe2d4316e3c5b8e8",
+    "url": "/static/media/cows.91ddc8b3.jpg"
+  },
+  {
+    "revision": "3bb4ac0ee2598a91579fabda9aac38df",
+    "url": "/static/media/market.3bb4ac0e.svg"
+  },
+  {
+    "revision": "51ec70441b0755347fe458d724d9132d",
+    "url": "/static/media/order-history.51ec7044.svg"
+  },
+  {
+    "revision": "43350967ae193656032dd72cdfac61e8",
+    "url": "/static/media/order.43350967.svg"
+  },
+  {
+    "revision": "723985adbdd345411a5d9f199b105370",
+    "url": "/static/media/sheep.723985ad.jpg"
+  },
+  {
+    "revision": "09a1068c08378cb85bdabe5d4ce90b33",
+    "url": "/static/media/straight-logo-no-icon.09a1068c.svg"
+  }
+]);
